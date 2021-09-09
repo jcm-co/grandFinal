05ee238403b949c35d19e12bb9f4a800a13fb7ac
jcm-co <76464497+jcm-co@users.noreply.github.com>
2021-09-09 20:03:14 +1000

    Update TokenSale

--- TokenSale.sol
+++ TokenSale.sol
@@ -1,36 +1,21 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.6.7;
 
-//Do we need to do SafeMath? If so then import from OpenZeppelin and adjust -=+ etc
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
 
-contract GrandFinalToken {
+
+contract GrandFinalToken is ERC20,ERCDetailed, ERC20Mintable {
+
     address payable public owner = msg.sender;
     //sender is the employer
-    string public symbol = "GFT";
-    uint public exchange_rate = 1;
-    //need to set exchange rate for how many tokens to employee per unit payment/bonus..
-
-    mapping(address => uint) balances;
-    // allows both employer and employee to see the balance of tokens
-
-    function balance() public view returns(uint) {
-        return balances[msg.sender];
-        // returning balnace of employer/employee
-    }
-
-    function transfer(address recipient, uint value) public {
-        balances[msg.sender] -= value;
-        balances[recipient] += value;
-    }
-
-    //need to link the "purchase/gift" with the employees salary without taking payment for the tokens
-    function purchase() public payable {
-        uint amount = msg.value * exchange_rate;
-        balances[msg.sender] += amount;
-        owner.transfer(msg.value);
-    }
-
-    function mint(address recipient, uint value) public {
-        require(msg.sender == owner, "You are not DE!");
-        balances[recipient] += value;
+    string public symbol;
+    
+    constructor(string memory name, string memory symbol, uint initial_supply) ERCDetailed(name, symbol, 18) public {
+        
+         //ArcadeToken, ARCD, 100000
+         
+         mint(msg.sender,initial_supply);
+        
     }
 }
